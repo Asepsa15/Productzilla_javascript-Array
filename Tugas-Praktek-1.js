6f4474be58c26c03a0f4a7752b7cbee8ec593a94
Asepsa15 <asepsaripudin2002@gmail.com>
2023-03-07 21:48:25 +0700

    Tugas-array

---
+++ Tugas-Praktek-1.js
@@ -0,0 +1,10 @@
+let wadah1 = ['jeruk', 'apel'];
+let wadah2 = ['semangka', 'pepaya'];
+let wadah3 = ['mangga'];
+
+// mengambil elemen dengan indeks ganjil
+let wadah = [];
+wadah.push(wadah1[0], wadah2[0], wadah3[0]);
+
+console.log('ganjil :');
+console.log(wadah);
---
+++ Tugas-Praktek-2.js
@@ -0,0 +1,10 @@
+let wadah1 = ['jeruk', 'apel'];
+let wadah2 = ['semangka', 'pepaya'];
+let wadah3 = ['mangga'];
+
+// mengambil elemen dengan indeks genap
+let wadah = [];
+wadah.push(wadah1[1], wadah2[1]);
+
+console.log('genap :');
+console.log(wadah);
